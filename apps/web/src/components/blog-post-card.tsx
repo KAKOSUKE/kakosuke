a7306bc2ad4b779b1c1f34625f0e685cd8c906fd
wellwich <mail@wellwich.com>
2025-07-12 11:34:32 +0900

    description を省略するように変更

--- apps/web/src/components/blog-post-card.tsx
+++ apps/web/src/components/blog-post-card.tsx
@@ -13,10 +13,11 @@ export default function BlogPostCard({
 		<Link
 			to="/blog/$slug"
 			params={{ slug }}
-			className="font-semibold text-blue-600 text-xl "
+			className="font-semibold text-blue-600 text-xl"
 		>
 			<div className="font-semibold text-lg">{title}</div>
-			<div className="mt-2 text-gray-600 text-sm dark:text-gray-400">
+			{/* 32文字以内で要約 */}
+			<div className="mt-2 overflow-hidden overflow-ellipsis text-gray-600 text-sm dark:text-gray-400">
 				{description}
 			</div>
 		</Link>
